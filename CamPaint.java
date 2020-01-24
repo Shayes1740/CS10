b6e41dc520f8bd876c8c543e68519693a3b1a115
Shayes1740 <47762096+Shayes1740@users.noreply.github.com>
2020-01-23 20:33:55 -0500

    Add files via upload

--- CamPaint.java
+++ CamPaint.java
@@ -11,111 +11,116 @@ import javax.swing.*;
  * @author Chris Bailey-Kellogg, Spring 2015 (based on a different webcam app from previous terms)
  */
 public class CamPaint extends Webcam {
-	private char displayMode = 'w';			// what to display: 'w': live webcam, 'r': recolored image, 'p': painting
-	private RegionFinder finder;			// handles the finding
-	private Color targetColor;          	// color of regions of interest (set by mouse press)
-	private Color paintColor = Color.blue;	// the color to put into the painting from the "brush"
-	private BufferedImage painting;			// the resulting masterpiece
+    private char displayMode = 'w';			// what to display: 'w': live webcam, 'r': recolored image, 'p': painting
+    private RegionFinder finder;			// handles the finding
+    private Color targetColor;          	// color of regions of interest (set by mouse press)
+    private Color paintColor = Color.blue;	// the color to put into the painting from the "brush"
+    private BufferedImage painting;			// the resulting masterpiece
 
-	/**
-	 * Initializes the region finder and the drawing
-	 */
-	public CamPaint() {
-		finder = new RegionFinder();
-		clearPainting();
-	}
+    /**
+     * Initializes the region finder and the drawing
+     */
+    public CamPaint() {
+        finder = new RegionFinder();
+        clearPainting();
+    }
 
-	/**
-	 * Resets the painting to a blank image
-	 */
-	protected void clearPainting() {
-		painting = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-	}
+    /**
+     * Resets the painting to a blank image
+     */
+    protected void clearPainting() {
+        painting = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    }
 
-	/**
-	 * DrawingGUI method, here drawing one of live webcam, recolored image, or painting,
-	 * depending on display variable ('w', 'r', or 'p')
-	 */
-	@Override
-	public void draw(Graphics g) {
+    /**
+     * DrawingGUI method, here drawing one of live webcam, recolored image, or painting,
+     * depending on display variable ('w', 'r', or 'p')
+     */
+    @Override
+    public void draw(Graphics g) {
+        // TODO: YOUR CODE HERE
 
-		if (image != null){
-			processImage();
-			if (displayMode == 'w'){
-				super.draw(g);
-			}
-			else if (displayMode == 'r'){
-				g.drawImage(finder.getRecoloredImage(), 0, 0, null);
-			}
-			else if(displayMode == 'p'){
-				g.drawImage(painting, 0, 0, null);
-			}
-		}
-		//else System.out.println("it is null");
+        if (image != null){
+            processImage();
+            if (displayMode == 'w'){
+                super.draw(g);
+            }
+            else if (displayMode == 'r'){
+                g.drawImage(finder.getRecoloredImage(), 0, 0, null);
+            }
+            else if(displayMode == 'p'){
+                g.drawImage(painting, 0, 0, null);
+            }
+        }
+        //else System.out.println("it is null");
 
-	}
+    }
 
-	/**
-	 * Overrides the DrawingGUI method to set the track color.
-	 */
-	@Override
-	public void handleMousePress(int x, int y) {
-		targetColor = new Color(image.getRGB(x, y));
-		System.out.println("tracking " + targetColor);
-	}
+    /**
+     * Overrides the DrawingGUI method to set the track color.
+     */
+    @Override
+    public void handleMousePress(int x, int y) {
+        // TODO: YOUR CODE HERE
+        targetColor = new Color(image.getRGB(x, y));
+        System.out.println("tracking " + targetColor);
+    }
 
-	/**
-	 * Webcam method, here finding regions and updating the painting.
-	 */
-	@Override
-	public void processImage() {
-		if (targetColor != null){
+    /**
+     * Webcam method, here finding regions and updating the painting.
+     */
+    @Override
+    public void processImage() {
+        // TODO: YOUR CODE HERE
+        if (targetColor != null){
 
-			finder.setImage(image);
-			finder.findRegions(targetColor);
+            finder.setImage(image);
+            finder.setRegions(new ArrayList<ArrayList<Point>>());
+            finder.findRegions(targetColor);
 
-			ArrayList<Point> finalRegion = finder.largestRegion();
-			for (Point points : finalRegion) {
-				if (painting.getRGB(points.x, points.y) != paintColor.getRGB()){
-					painting.setRGB(points.x, points.y, paintColor.getRGB());
-				}
-			}
-
-
-			finder.recolorImage();
-
-		}
-	}
+            ArrayList<Point> finalRegion = finder.largestRegion();
+            for (Point points : finalRegion) {
+                if (painting.getRGB(points.x, points.y) != paintColor.getRGB()){
+                    painting.setRGB(points.x, points.y, paintColor.getRGB());
+                }
+            }
 
 
-	/**
-	 * DrawingGUI method, here doing various drawing commands
-	 */
-	@Override
-	public void handleKeyPress(char k) {
-		if (k == 'p' || k == 'r' || k == 'w') { // display: painting, recolored image, or webcam
-			displayMode = k;
-			System.out.println("Key press is on '" +displayMode+ "'");
-		}
-		else if (k == 'c') { // clear
-			clearPainting();
-		}
-		else if (k == 'o') { // save the recolored image
-			saveImage(finder.getRecoloredImage(), "pictures/recolored.png", "png");
-		}
-		else if (k == 's') { // save the painting
-			saveImage(painting, "pictures/painting.png", "png");
-		}
-		else {
-			System.out.println("unexpected key "+k);
-		}
-	}
+            finder.recolorImage();
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				new CamPaint();
-			}
-		});
-	}
+        }
+    }
+
+
+    /**
+     * DrawingGUI method, here doing various drawing commands
+     */
+    @Override
+    public void handleKeyPress(char k) {
+        if (k == 'p' || k == 'r' || k == 'w') { // display: painting, recolored image, or webcam
+            displayMode = k;
+            System.out.println("Key press is on '" +displayMode+ "'");
+        }
+        else if (k == 'c') { // clear
+            clearPainting();
+        }
+        else if (k == 'o') { // save the recolored image
+            saveImage(finder.getRecoloredImage(), "pictures/recolored.png", "png");
+        }
+        else if (k == 's') { // save the painting
+            saveImage(painting, "pictures/painting.png", "png");
+        }
+        else {
+            System.out.println("unexpected key "+k);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new CamPaint();
+            }
+        });
+    }
 }
+
--- RegionFinder.java
+++ RegionFinder.java
@@ -12,128 +12,136 @@ import java.util.*;
  * @author CBK, Spring 2015, updated for CamPaint
  */
 public class RegionFinder {
-	private static final int maxColorDiff = 200;            // how similar a pixel color must be to the target color, to belong to a region
-	private static final int minRegion = 50;                // how many points in a region to be worth considering
-	private static final int radius = 1;
+    private static final int maxColorDiff = 500;            // how similar a pixel color must be to the target color, to belong to a region
+    private static final int minRegion = 50;               // how many points in a region to be worth considering
+    private static final int radius = 1;
 
-	private BufferedImage image;                            // the image in which to find regions
-	private BufferedImage recoloredImage;                   // the image with identified regions recolored
+    private BufferedImage image;                            // the image in which to find regions
+    private BufferedImage recoloredImage;                   // the image with identified regions recolored
 
-	private ArrayList<ArrayList<Point>> regions;            // a region is a list of points
-	// so the identified regions are in a list of lists of points
-	private ArrayList<Point> innerRegion;
-	private ArrayList<Point> toVisit;
+    private ArrayList<ArrayList<Point>> regions = new ArrayList<ArrayList<Point>>();;            // a region is a list of points
+    // so the identified regions are in a list of lists of points
+    private ArrayList<Point> innerRegion;
+    private ArrayList<Point> toVisit;
 
 
-	public RegionFinder() {
-		this.image = null;
-	}
+    public RegionFinder() {
+        this.image = null;
+    }
 
-	public RegionFinder(BufferedImage image) {
-		this.image = image;
-	}
+    public RegionFinder(BufferedImage image) {
+        this.image = image;
+    }
 
-	public void setImage(BufferedImage image) {
-		this.image = image;
-	}
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 
-	public BufferedImage getImage() {
-		return image;
-	}
+    public void setRegions (ArrayList<ArrayList<Point>> regions){
+        this.regions = regions;
+    }
 
-	public BufferedImage getRecoloredImage() {
-		return recoloredImage;
-	}
+    public BufferedImage getImage() {
+        return image;
+    }
 
-	/**
-	 * Sets regions to the flood-fill regions in the image, similar enough to the trackColor.
-	 */
-	public void findRegions(Color targetColor) {
-		BufferedImage visited = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-		regions = new ArrayList<ArrayList<Point>>();
-		for (int y = 0; y < image.getHeight(); y++) {
-			for (int x = 0; x < image.getWidth(); x++) {
-				innerRegion = new ArrayList<Point>();
-				toVisit = new ArrayList<Point>();
+    public BufferedImage getRecoloredImage() {
+        return recoloredImage;
+    }
 
-				if(visited.getRGB(x,y) == 0 && colorMatch(targetColor, new Color(image.getRGB(x,y)))){
-					toVisit.add(new Point(x, y));
-				}
+    /**
+     * Sets regions to the flood-fill regions in the image, similar enough to the trackColor.
+     */
+    public void findRegions(Color targetColor) {
+        // TODO: YOUR CODE HERE
+        BufferedImage visited = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-				while (toVisit.size() > 0) {
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                innerRegion = new ArrayList<Point>();
+                toVisit = new ArrayList<Point>();
 
-					int nx = toVisit.get(toVisit.size()-1).x;
-					int ny = toVisit.get(toVisit.size()-1).y;
+                if(visited.getRGB(x,y) == 0 && colorMatch(targetColor, new Color(image.getRGB(x,y)))){
+                    toVisit.add(new Point(x, y));
+                }
 
-					Color checkedColor = new Color(image.getRGB(nx, ny));
-					if (visited.getRGB(nx, ny) == 0 && colorMatch(checkedColor, targetColor)){
-						for (int b = Math.max(0, ny - radius); b < Math.min(image.getHeight(), ny + radius + 1); b++) {
-							for (int a = Math.max(0, nx - radius); a < Math.min(image.getWidth(), nx + radius + 1); a++) {
-								toVisit.add(new Point(a, b));
+                while (toVisit.size() > 0) {
 
-							}
-						}
+                    int nx = toVisit.get(0).x;
+                    int ny = toVisit.get(0).y;
 
-						innerRegion.add(toVisit.get(toVisit.size()-1));
-					}
-					visited.setRGB(nx, ny, 1);
-					toVisit.remove(toVisit.get(toVisit.size()-1));
+                    Color checkedColor = new Color(image.getRGB(nx, ny));
+                    if (visited.getRGB(nx, ny) == 0 && colorMatch(checkedColor, targetColor)){
+                        for (int b = Math.max(0, ny - radius); b < Math.min(image.getHeight(), ny + radius + 1); b++) {
+                            for (int a = Math.max(0, nx - radius); a < Math.min(image.getWidth(), nx + radius + 1); a++) {
+                                toVisit.add(new Point(a, b));
+                            }
+                        }
+                        innerRegion.add(toVisit.get(0));
+                    }
+                    visited.setRGB(nx, ny, 1);
+                    if (toVisit.size() > 0) {
+                        toVisit.remove(toVisit.get(0));
+                    }
 
-				}
-				if (innerRegion.size() >= minRegion) {
-					regions.add(innerRegion);
-				}
+                }
+                if (innerRegion.size() >= minRegion) {
+                    regions.add(innerRegion);
+                }
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	/**
-	 * Tests whether the two colors are "similar enough" (your definition, subject to the maxColorDiff threshold, which you can vary).
-	 */
-	private static boolean colorMatch (Color c1, Color c2) {
-		int diff = (c1.getRed() - c2.getRed()) * (c1.getRed() - c2.getRed())
-				+ (c1.getGreen() - c2.getGreen()) * (c1.getGreen() - c2.getGreen())
-				+ (c1.getBlue() - c2.getBlue()) * (c1.getBlue() - c2.getBlue());
-		return (diff < maxColorDiff);
-	}
+    /**
+     * Tests whether the two colors are "similar enough" (your definition, subject to the maxColorDiff threshold, which you can vary).
+     */
+    private static boolean colorMatch (Color c1, Color c2) {
+        // TODO: YOUR CODE HERE
+        int diff = (c1.getRed() - c2.getRed()) * (c1.getRed() - c2.getRed())
+                + (c1.getGreen() - c2.getGreen()) * (c1.getGreen() - c2.getGreen())
+                + (c1.getBlue() - c2.getBlue()) * (c1.getBlue() - c2.getBlue());
+        return (diff < maxColorDiff);
+    }
 
-	/**
-	 * Returns the largest region detected (if any region has been detected)
-	 */
-	public ArrayList<Point> largestRegion() {
-		ArrayList<Point> largestOutThere = new ArrayList<Point>();
-		for(ArrayList<Point> region: regions){
-			if (region.size() > largestOutThere.size()){
-				largestOutThere = region;
-			}
-		}
-		return largestOutThere;
-	}
+    /**
+     * Returns the largest region detected (if any region has been detected)
+     */
+    public ArrayList<Point> largestRegion() {
+        // TODO: YOUR CODE HERE
+        ArrayList<Point> largestOutThere = new ArrayList<Point>();
+        for(ArrayList<Point> region: regions){
+            if (region.size() > largestOutThere.size()){
+                largestOutThere = region;
+            }
+        }
+        return largestOutThere;
+    }
 
-	/**
-	 * Sets recoloredImage to be a copy of image,
-	 * but with each region a uniform random color,
-	 * so we can see where they are
-	 */
-	public void recolorImage() {
-		// First copy the original
-		try {
-			recoloredImage = new BufferedImage(image.getColorModel(), image.copyData(null), image.getColorModel().isAlphaPremultiplied(), null);
-			// Now recolor the regions in it
-			int maxColorValue = 16777216;
-			int chosenColor;
+    /**
+     * Sets recoloredImage to be a copy of image,
+     * but with each region a uniform random color,
+     * so we can see where they are
+     */
+    public void recolorImage() {
+        // First copy the original
+        try {
+            recoloredImage = new BufferedImage(image.getColorModel(), image.copyData(null), image.getColorModel().isAlphaPremultiplied(), null);
+            // Now recolor the regions in it
+            // TODO: YOUR CODE HERE
+            int maxColorValue = 16777216;
+            int chosenColor;
 
-			for (ArrayList<Point> region : regions) {
-				chosenColor = (int) (Math.random() * (maxColorValue + 1)); //generates random number in range of colors
-				Color color = new Color(chosenColor); //creates new color with chosen color value
-				for (Point points : region) {
-					recoloredImage.setRGB(points.x, points.y, color.getRGB());
-				}
-			}
-		}
-		catch(Exception e){
-			System.out.println("too slow");
-		}
-	}
+            for (ArrayList<Point> region : regions) {
+                chosenColor = (int) (Math.random() * (maxColorValue + 1)); //generates random number in range of colors
+                Color color = new Color(chosenColor); //creates new color with chosen color value
+                for (Point points : region) {
+                    recoloredImage.setRGB(points.x, points.y, color.getRGB());
+                }
+            }
+        }
+        catch(Exception e){
+            System.out.println("too slow");
+        }
+    }
 }
