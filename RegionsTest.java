93fb734232870e4ab3882b8cc9e87e51097ba159
TJFyda <59985871+TJFyda@users.noreply.github.com>
2020-01-23 21:10:23 -0500

    Add files via upload

--- RegionsTest.java
+++ RegionsTest.java
@@ -10,34 +10,36 @@ import javax.swing.*;
  * @author Travis W. Peters, Dartmouth CS 10, Updated Winter 2015
  * @author CBK, Spring 2015, updated for DrawingGUI
  */
+
 public class RegionsTest extends DrawingGUI {
-    private BufferedImage image;
+	private BufferedImage image;
 
-    /**
-     * Test your RegionFinder by passing an image filename and a color to find.
-     * @param filename
-     * @param color
-     */
-    public RegionsTest(String name, RegionFinder finder, Color targetColor) {
-        super(name, finder.getImage().getWidth(), finder.getImage().getHeight());
+	/**
+	 * Test your RegionFinder by passing an image filename and a color to find.
+	 * @param filename
+	 * @param color
+	 */
 
-        // Do the region finding and recolor the image.
-        finder.findRegions(targetColor);
-        finder.recolorImage();
-        image = finder.getRecoloredImage();
-    }
+	public RegionsTest(String name, RegionFinder finder, Color targetColor) {
+		super(name, finder.getImage().getWidth(), finder.getImage().getHeight());
 
-    @Override
-    public void draw(Graphics g) {
-        g.drawImage(image, 0, 0, null);
-    }
+		// Do the region finding and recolor the image.
+		finder.findRegions(targetColor);
+		finder.recolorImage();
+		image = finder.getRecoloredImage();
+	}
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                //new RegionsTest("smiley", new RegionFinder(loadImage("pictures/smiley.png")), new Color(0, 0, 0));
-                new RegionsTest("baker", new RegionFinder(loadImage("pictures/baker.jpg")), new Color(130, 100, 100));
-            }
-        });
-    }
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(image, 0, 0, null);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				//new RegionsTest("smiley", new RegionFinder(loadImage("pictures/smiley.png")), new Color(0, 0, 0));
+				new RegionsTest("baker", new RegionFinder(loadImage("pictures/baker.jpg")), new Color(130, 100, 100));
+			}
+		});
+	}
 }
