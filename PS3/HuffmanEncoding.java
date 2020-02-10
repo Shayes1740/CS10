df6fbdaf86ca7f16ec09137583ab90c26ed85ab5
TJFyda <59985871+TJFyda@users.noreply.github.com>
2020-02-10 18:47:32 -0500

    Add files via upload

--- PS3/HuffmanEncoding.java
+++ PS3/HuffmanEncoding.java
@@ -29,39 +29,85 @@ public class HuffmanEncoding {
             System.err.println("Cannot open file.\n" + e.getMessage());
             return wordCounts;
         }
+        try {
+            current = input.read();
+        }
+        catch (IOException e1) {
+            System.out.println("Empty File");
+        }
+
+        while (current != -1) {
+            char character = (char) current;
+            if (wordCounts.containsKey(character)){
+                wordCounts.put(character, wordCounts.get(character) + 1);
+            }
+            else {
+                wordCounts.put(character, 1);
+            }
             try {
                 current = input.read();
             }
             catch (IOException e1) {
                 System.out.println("Empty File");
             }
-
-            while (current != -1) {
-                char character = (char) current;
-                if (wordCounts.containsKey(character)){
-                    wordCounts.put(character, wordCounts.get(character) + 1);
-                }
-                else {
-                    wordCounts.put(character, 1);
-                }
-                try {
-                    current = input.read();
-                }
-                catch (IOException e1) {
-                    System.out.println("Empty File");
-                }
-            }
-            return wordCounts;
+        }
+        return wordCounts;
     }
 
     /**
      * Priority queue
      */
 
+    public PriorityQueue<TreeData> priorityQueue () {
+        Comparator<TreeData> treeCompare = new TreeComparator();
+        PriorityQueue<TreeData> pQueue = new PriorityQueue<TreeData>(frequencyTable().size(), treeCompare);
+        for (Character k : frequencyTable().keySet()) {
+            TreeData initTree = new TreeData(k, frequencyTable().get(k));
+            pQueue.add(initTree);
+        }
+
+        while (pQueue.size() > 1) {
+            TreeData T1 = pQueue.peek();
+            pQueue.remove(T1);
+
+            TreeData T2 = pQueue.peek();
+            pQueue.remove(T2);
+
+            Character root = '▒';
+
+            TreeData newTree = new TreeData(root, (T1.getValue() + T2.getValue()), T1, T2);
+            pQueue.add(newTree);
+
+        }
+
+        return pQueue;
+    }
+
     /**
      * Code retrieval
      */
 
+//    public Map<Character, Integer> codeTree () {
+//        TreeData characterTree = priorityQueue().poll();
+//
+//
+//        return;
+//    }
+//
+////    public BinaryTree<E> copyToDepth(int d) {
+////        if (d == 0) {
+////            return new BinaryTree<E>(data);
+////        }
+////        BinaryTree<E> l = null;
+////        BinaryTree<E> r = null;
+////        if (hasLeft()) {
+////            l = left.copyToDepth(d-1);
+////        }
+////        if (hasRight()) {
+////            r = right.copyToDepth(d-1);
+////        }
+////        return new BinaryTree<E>(data, l, r);
+////    }
 
     /**
      * Compression
